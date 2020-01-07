2b3d7969d3be9d83e8854bc961fb304aefecebe3
HigorSegobia <higor.segobia@gmail.com>
2020-01-07 13:04:50 -0300

    Modulo1

---
+++ curso-js/exe4.js
@@ -0,0 +1,15 @@
+function experiencia(anos) {
+    if(anos <= 1){
+        console.log('Iniciante')
+    }else if(anos <=5){
+        console.log('Junior')
+    }else if (anos <=9){
+        console.log('Pleno')
+    }else if (anos > 10){
+        console.log('Senior')
+    }else;
+   }
+   var anosEstudo = 12;
+   experiencia(anosEstudo);
+ 
+   
---
+++ curso-js/exe5.js
@@ -0,0 +1,20 @@
+function mostraHabilidade(usuarios){
+    for(usuario of usuarios){
+        console.log("O " + usuario.nome + " possui as habilidades " + usuario.habilidades.join(", "))
+    };
+}
+
+
+
+var usuarios = [
+    {
+    nome: "Diego",
+    habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+    nome: "Gabriel",
+    habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+   ];
+   
+   mostraHabilidade(usuarios);
